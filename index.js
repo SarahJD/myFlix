9ccc09bc68116b6bc69f6961674a11582bfbeeb7
SarahJD <sarahjdudek@gmail.com>
2020-12-16 14:04:05 +0100

    installed Morgan as an Express logging middleware

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // install Express
 const express = require('express');
 const app = express();
+
+// install Morgan as an Express logging middleware
+const morgan = require('morgan');
+
+app.use(morgan('common'));
+
+// Express GET routes
+app.get('/movies', (req, res) => {
+  res.json(top10Movies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
